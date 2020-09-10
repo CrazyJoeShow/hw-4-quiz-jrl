329e40a12e73f5d654e7b5214b97222c2374c546
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-09 21:15:00 -0400

    possible answers and socre tracker

--- quiz.js
+++ quiz.js
@@ -10,3 +10,13 @@ var quizContainer = document.querySelector('#disappearDiv');
 //starts timer and quiz
 var timerButton = document.querySelector('#timerStart');
 var subHeadDisp = document.querySelector('#subHeadingMain');
+var mainHeadDisp = document.querySelector('#headingMain');
+//answers IDdivs
+
+var possAnswer1 = document.querySelector('#answer1');
+var possAnswer2 = document.querySelector('#answer2');
+var possAnswer3 = document.querySelector('#answer3');
+var possAnswer4 = document.querySelector('#answer4');
+
+var currentPage = 0;
+var trackedScore = 0;
